828b044086ad5c8b7777f6c244698ee2abac936d
Yuri Matheus <yurimatheusb@hotmail.com>
2018-08-26 11:23:57 -0300

    adiciona a estrutura de pastas do projeto

---
+++ src/main/java/br/com/fiap/logisticaatento/LogisticaAtentoApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.logisticaatento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaAtentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaAtentoApplication.class, args);
+	}
+}
