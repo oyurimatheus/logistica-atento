e98d24ada30eb17d5b9052ac51002c70a6e78249
Gabriel <gabriheron@hotmail.com>
2018-09-03 18:43:35 -0300

    [Dados] Criando classe que contem dados do endereço

---
+++ src/main/java/br/com/fiap/logisticaatento/dados/Endereco.java
@@ -0,0 +1,53 @@
+package br.com.fiap.logisticaatento.modelo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+
+    private String cep;
+
+    private String nomeDaRua;
+
+    private int numero;
+    private String complemento;
+
+    public Endereco(String cep, String nomeDaRua, int numero, String complemento) {
+        this.cep = cep;
+        this.nomeDaRua = nomeDaRua;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNomeDaRua() {
+        return nomeDaRua;
+    }
+
+    public void setNomeDaRua(String nomeDaRua) {
+        this.nomeDaRua = nomeDaRua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
