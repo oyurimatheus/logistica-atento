48241e7ed549ef061cea265967005e8f21895fc3
Gabriel <gabriheron@hotmail.com>
2018-09-03 18:45:52 -0300

    [Modelo] Criando classe modelo de Funcionario

---
+++ src/main/java/br/com/fiap/logisticaatento/modelo/Funcionario.java
@@ -0,0 +1,64 @@
+package br.com.fiap.logisticaatento.modelo;
+
+import java.time.LocalTime;
+import br.com.fiap.logisticaatento.modelo.Nome;
+import br.com.fiap.logisticaatento.modelo.Endereco;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionario {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Embedded
+    private Nome nome;
+
+    @Embedded
+    private Endereco endereco;
+    private LocalTime horaDeTrabalho;
+
+    public Funcionario(long id, Nome nome, Endereco endereco, LocalTime horaDeTrabalho) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.horaDeTrabalho = horaDeTrabalho;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Nome getNome() {
+        return nome;
+    }
+
+    public void setNome(Nome nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalTime getHoraDeTrabalho() {
+        return horaDeTrabalho;
+    }
+
+    public void setHoraDeTrabalho(LocalTime horaDeTrabalho) {
+        this.horaDeTrabalho = horaDeTrabalho;
+    }
+}
